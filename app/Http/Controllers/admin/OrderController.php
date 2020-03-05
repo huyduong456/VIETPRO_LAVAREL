a16756c6e7d243a4e937bb125421f69e916023d7
huyduong456 <huyduong45615@gmail.com>
2020-03-05 19:03:27 +0700

    buoi 2

--- app/Http/Controllers/admin/OrderController.php
+++ app/Http/Controllers/admin/OrderController.php
@@ -7,5 +7,24 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    public function processed()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,80 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group([
+    'prefix' => 'admin',
+    'namespace' => 'Admin'
+
+], function () {
+    Route::group(['prefix' => 'products'], function(){
+        Route::get('', 'ProductController@index');
+        Route::get('create', 'ProductController@create'); // sau @ là tên function
+        Route::get('', 'ProductController@store');
+        Route::get('{product}/edit', 'ProductController@edit');
+        Route::put('{product}', 'ProductController@update');
+        Route::delete('{product}', 'ProductController@destroy');
+    });
+    Route::resource('users', 'UserController');
+});
+Route::group([
+    'prefix' => 'admin',
+    'namespace' => 'Admin'
+], function () {
+    Route::group(['prefix' => 'category'], function () {
+        Route::get('', 'CategoryController@index');
+        Route::get('create', 'CategoryController@create');
+        Route::get('', 'CategoryController@store');
+        Route::get('{category}/eidt', 'CategoryController@edit');
+        Route::get('{category}', 'CategoryController@update');
+        Route::delete('{category}', 'CategoryController@destroy');
+    });
+});
+
+Route::group([
+    'prefix' => 'admin',
+    'namespace' => 'Admin'
+], function () {
+    Route::group(['prefix' => 'dashboard'], function () {
+        Route::get('', 'DashboardController@dashboard');
+    });
+});
+
+Route::group([
+    'prefix' => 'admin',
+    'namespace' => 'Admin'
+], function () {
+    Route::group(['prefix' => 'LoginController'], function () {
+        Route::get('', 'LoginController@showLoginForm');
+        Route::get('login', 'LoginController@login');
+        Route::get('logout', 'LoginController@logout');
+    });
+});
+
+Route::group([
+    'prefix' => 'admin',
+    'namespace' => 'Admin'
+], function () {
+    Route::group(['prefix' => 'OrderController'], function () {
+        Route::get('', 'OrderController@index');
+        Route::get('processed', 'OrderController@processed');
+        Route::get('{OrderController}edit', 'OrderController@edit');
+        Route::get('{OrderController}update', 'OrderController@update');
+    });
+});
+
+Route::group([
+    'prefix' => 'admin',
+    'namespace' => 'Admin'
+], function () {
+    Route::group(['prefix' => 'category'], function () {
+        Route::get('', 'CategoryController@index');
+        Route::get('create', 'CategoryController@create');
+        Route::get('', 'CategoryController@store');
+        Route::get('{category}/eidt', 'CategoryController@edit');
+        Route::get('{category}', 'CategoryController@update');
+        Route::delete('{category}', 'ProductController@destroy');
+    });
+});
+
+
